dbf1a1e7f5bb6c0fc4f6ee3f759fbad0e693f956
Justin Derby <justderb@gmail.com>
2022-04-11 18:26:01 -0700

    Initial commit

---
+++ CursorFreedom.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine;
+
+// Allow scanning for ConCommand, and other stuff for Risk of Rain 2
+[assembly: HG.Reflection.SearchableAttribute.OptIn]
+namespace CursorFreedom
+{
+    [BepInPlugin(GUID, ModName, Version)]
+    public class CursorFreedom : BaseUnityPlugin
+    {
+        public const string GUID = "com.justinderby.cursorfreedom";
+        public const string ModName = "CursorFreedom";
+        public const string Version = "1.0.0";
+
+        public static CursorFreedom Instance;
+
+        public void Awake()
+        {
+            On.RoR2.MPEventSystemManager.Update += MPEventSystemManager_Update;
+            On.RoR2.RoR2Application.Update += RoR2Application_Update;
+        }
+
+        public void Destroy()
+        {
+            On.RoR2.MPEventSystemManager.Update -= MPEventSystemManager_Update;
+            On.RoR2.RoR2Application.Update -= RoR2Application_Update;
+        }
+
+        private void RoR2Application_Update(On.RoR2.RoR2Application.orig_Update orig, RoR2Application self)
+        {
+            orig(self);
+            PossiblyFreeCursor();
+        }
+
+        private void MPEventSystemManager_Update(On.RoR2.MPEventSystemManager.orig_Update orig, MPEventSystemManager self)
+        {
+            orig(self);
+            PossiblyFreeCursor();
+        }
+
+        private void PossiblyFreeCursor()
+        {
+            if (!Application.isBatchMode)
+            {
+                Cursor.lockState = ((MPEventSystemManager.kbmEventSystem.isCursorVisible || MPEventSystemManager.combinedEventSystem.isCursorVisible) ? CursorLockMode.None : CursorLockMode.Locked);
+                Cursor.visible = false;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Cursor Freedom
+
+Changes the game settings so that in windowed mode your cursor is free to exit the window.
+
+# Changelog
+
+### 1.0.0
+
+* Initial release
